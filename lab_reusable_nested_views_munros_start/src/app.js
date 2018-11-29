3f7c057b4031b46a3f6d6ea3a8d75fac0e3a1c92
Catriona Ford <catriona.b.ford@gmail.com>
2018-11-29 12:12:56 +0000

    app.js worked on, file structure added

--- lab_reusable_nested_views_munros_start/src/app.js
+++ lab_reusable_nested_views_munros_start/src/app.js
@@ -1,3 +1,9 @@
+const MunrosListView = require('./views/munros_list_view.js')
+const Munros = require('./models/munros.js')
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+  const munrosListContainer = document.querySelector('#munros');
+  const munrosListView = new MunrosListView(munrosListContainer);
+  munrosListView.bindEvents();
 })
